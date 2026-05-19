5c9d0779a5d6d3d00a601fd98e4068c3ea1cb75d
Hugo González Martos <hugo.gonzalez.martos.aldiespoligonosur.org>
2026-05-19 17:03:24 +0200

    Comentarios

---
+++ 1/comentarios.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Prácticas PHP</title>
+</head>
+
+<body>
+
+<?php
+
+    //Soy un comentario
+    /* Soy un comentario
+       de varias
+       lineas
+    */
+    # Comentario de tipo Unix
+
+    echo "Hola Mundo";
+
+?>
+
+</body>
+</html>
