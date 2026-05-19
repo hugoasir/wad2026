d4115d85a5381d0a0deb127ef5f0732d8e910284
Hugo González Martos <hugo.gonzalez.martos.aldiespoligonosur.org>
2026-05-19 17:13:47 +0200

    variable

---
+++ 1/variables.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Prácticas PHP</title>
+</head>
+
+<body>
+
+<?php
+
+    //Variables
+    $edad = 39;
+    $estatura = 1.67;
+    $nombre = "Luis Javier";
+    $frase = "Luisja tiene $edad años";
+    $frase2 = 'Luisja tiene $edad años';
+    $profesor = true;
+
+    echo $edad;
+
+?>
+
+</body>
+</html>
