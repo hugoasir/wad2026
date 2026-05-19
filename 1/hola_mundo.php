f22e0da93ffdacf0cd886e8977aeb7d243378a21
Hugo González Martos <hugo.gonzalez.martos.aldiespoligonosur.org>
2026-05-19 16:59:48 +0200

    Actualización README

---
+++ 1/hola_mundo.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Prácticas PHP</title>
+</head>
+
+<body>
+
+<?php
+    echo "Hola Mundo";
+?>
+
+</body>
+</html>
---
+++ README.md
@@ -0,0 +1,4 @@
+# wad2026
+## Este es el repositorio de Hugo González Martos para el módulo de Implantación de Aplicaciones Web
+### pruebaaa
+#### pruebaaa
